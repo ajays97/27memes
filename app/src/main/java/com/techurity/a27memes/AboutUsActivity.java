47b2414ffe6d2a054ba0268187e9e907686aa912
Ajay Srinivas <ajaysrinivas530@gmail.com>
2017-06-08 07:09:47 +0530

    Added About Us/Support Us.

--- app/src/main/java/com/techurity/a27memes/AboutUsActivity.java
+++ app/src/main/java/com/techurity/a27memes/AboutUsActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.reward.RewardItem;
 import com.google.android.gms.ads.reward.RewardedVideoAd;
@@ -17,13 +18,16 @@ public class AboutUsActivity extends AppCompatActivity implements RewardedVideoA
 
     Button watchAd;
     private RewardedVideoAd mAd;
+    private AdView adView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        MobileAds.initialize(this, "ca-app-pub-2819514375619003~1342858770");
 
         watchAd = (Button) findViewById(R.id.supportBtn);
+        adView = (AdView) findViewById(R.id.supportAd);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -39,6 +43,9 @@ public class AboutUsActivity extends AppCompatActivity implements RewardedVideoA
                     mAd.show();
             }
         });
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
 
     }
 
@@ -62,6 +69,7 @@ public class AboutUsActivity extends AppCompatActivity implements RewardedVideoA
     @Override
     public void onRewardedVideoAdFailedToLoad(int errorCode) {
         Toast.makeText(this, "Something Went Wrong", Toast.LENGTH_SHORT).show();
+        watchAd.setText("Could not load video");
     }
 
     @Override
