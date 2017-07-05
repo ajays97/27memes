faf36bc31b4b5397150ae22323f153f030d9eb79
Ajay Srinivas <ajay.s1997@gmail.com>
2017-07-05 20:38:52 +0530

    Working on Timeline Module.

--- app/src/main/java/com/techurity/a27memes/TimelineActivity.java
+++ app/src/main/java/com/techurity/a27memes/TimelineActivity.java
@@ -1,6 +1,7 @@
 package com.techurity.a27memes;
 
 import android.app.ProgressDialog;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -34,6 +35,7 @@ public class TimelineActivity extends AppCompatActivity {
     HashMap<String, GraphResponse> requestHashMap;
 
     GraphRequest listRequest;
+    GraphRequest request = null;
 
     ListView timelineList;
     ListAdapter timelineAdapter;
@@ -44,10 +46,7 @@ public class TimelineActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
-        pDialog = new ProgressDialog(this);
-        pDialog.setTitle("Timeline");
-        pDialog.setMessage("Loading fun...");
-        pDialog.show();
+
 
         requestHashMap = new HashMap<>();
 
@@ -57,7 +56,7 @@ public class TimelineActivity extends AppCompatActivity {
 
         loadList();
 
-        pDialog.dismiss();
+
 
     }
 
@@ -92,31 +91,45 @@ public class TimelineActivity extends AppCompatActivity {
 
     public void updateFeed(ArrayList<String> page_list, ArrayList<String> page_id_list) {
 
-        timelineAdapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, page_id_list);
-        timelineList.setAdapter(timelineAdapter);
-
         Bundle parameters = new Bundle();
         parameters.putString("limit", "1");
         parameters.putString("fields", "full_picture,message,created_time,type,link");
 
+        pDialog = new ProgressDialog(this);
+        pDialog.setTitle("Timeline");
+        pDialog.setMessage("Loading fun...");
+        pDialog.show();
+
         for(int i = 0; i< page_list.size(); i++){
 
             page = page_id_list.get(i);
-            GraphRequest request = new GraphRequest(AccessToken.getCurrentAccessToken(), "/" + page + "/feed", parameters, HttpMethod.GET, new GraphRequest.Callback() {
+            request = new GraphRequest(AccessToken.getCurrentAccessToken(), "/" + page + "/feed", parameters, HttpMethod.GET, new GraphRequest.Callback() {
                 @Override
                 public void onCompleted(GraphResponse response) {
                     responses.add(response);
                 }
             });
-            request.executeAsync();
-        }
-        Log.d("RESPONSES", responses.toString());
 
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            request.executeAndWait();
+
+        }
+        pDialog.dismiss();
+        Log.d("RESPONSES", responses.toString());
     }
 
     public void setPageDetails(ArrayList<String> names, ArrayList<String> ids) {
         page_names = names;
         page_ids = ids;
+
+        timelineAdapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, page_names);
+        timelineList.setAdapter(timelineAdapter);
+
+
+
         updateFeed(page_names, page_ids);
+
+
     }
 }
